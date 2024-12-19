4c68f760aba3ffbacfe662df106360e5d24152be
Siddharth <sahusidd715@gmail.com>
2024-12-19 08:26:04 +0530

    ui

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-6 px-24'>
+       <div className='flex items-center tracking-tight'>
+        <img src='vector.png'  className='h-6 w-6'/>
+        <h2 className='text-customBlack font-semibold text-xl font-poppins tracking-tight'>BrandNav</h2>
+       </div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className=''>
+            <h1 className='text-xl text-customBlue leading-6 font-medium font-poppins text-center'>Find fast-growing brands, not low quality stores</h1>
+            <div className='px-64 py-11'>
+                <h1 className='leading-10 font-bold text-customBlue2 text-5xl font-poppins text-center'>
+                    A Store Leads Alternative,<span className='text-customBlue'>10X Better</span>  in Every Way
+                </h1>
+            </div>
+
+            <h1 className='text-customBlue3 text-lg font-poppins text-center leading-6'>Discover untapped eCommerce brands with our high-quality unsaturated leads database</h1>
+            <div className='flex justify-center items-center gap-1'>
+                <input type='text' placeholder='Enter your Work email' className='w-96 h-11 rounded-md px-4 py-2 border border-customGray' />
+                <button className='bg-customBlue text-customWhite px-4 py-2 rounded-md'>Grab Free loads</button>
+            </div>
+            <div className='flex justify-center items-center gap-1'><div className='flex justify-center items-center gap-1'><img src="free.png" alt="" className='w-4 h-4' /><p className='text-customBlue3 text-sm font-medium'>5,000 free leads included</p> </div> |<div className='flex gap-1'><img src='credit.png' className='w-4 h-4' /><p className='text-customBlue3 text-sm font-medium'>No credit card required</p></div>  </div>
+        </div>
+    )
+}
+
+export default Hero
