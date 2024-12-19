8988421a8ab11967f86a8aba3992df7ddb8b3616
Siddharth <sahusidd715@gmail.com>
2024-12-19 10:37:44 +0530

    ...

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Section2 = () => {
   return (
     <div className='flex justify-center items-center bg-customBlue5 mb-3 p-3'>
-      <img src='section2.png' alt='section2' className='w-fit h-fit object-contain '/>
+      <img 
+        src='section2.png' 
+        alt='section2' 
+        className='w-fit h-fit object-contain select-none rendering-crisp-edges drop-shadow-[0_6px_30px_rgba(24,0,118,0.24)]'
+      />
     </div>
   )
 }
