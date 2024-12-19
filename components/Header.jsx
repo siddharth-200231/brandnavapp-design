232d5f38f1f03162b0ff4b33470e630ccad158be
Siddharth <sahusidd715@gmail.com>
2024-12-19 11:40:17 +0530

    font weight changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='py-4 px-4 sm:px-6 md:px-12 lg:px-24 z-50 shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-white'>
       <div className='flex items-center tracking-tight'>
         <img src='vector.png' alt='logo' className='h-6 w-6'/>
-        <h2 className='text-customBlack font-semibold text-xl font-poppins tracking-tight'>BrandNav</h2>
+        <h2 className='text-customBlack font-medium text-xl font-poppins tracking-tight'>BrandNav</h2>
       </div>
     </div>
   )
