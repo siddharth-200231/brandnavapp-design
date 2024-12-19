5e1f1262a72736c2a78401e0288fe3f1a7eeb407
Siddharth <sahusidd715@gmail.com>
2024-12-19 10:23:45 +0530

    Section2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Section2 from '../components/Section2'
 function page() {
   return (
     <div className='bg-customBlue5 h-screen flex flex-col'>
       <Header />
-      <div> <Hero /></div>
-     
+      
+      <Hero />
+      <Section2 />
     </div>
   )
 }
--- components/Section2.jsx
+++ components/Section2.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Section2 = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center bg-customBlue5 mb-3'>
+      <img src='section2.png' alt='section2' className='w-fit h-fit object-contain'/>
     </div>
   )
 }
