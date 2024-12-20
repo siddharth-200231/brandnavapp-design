13bf730c5d611892cd586ecb9fe3c7ba4cb80fdf
Siddharth <sahusidd715@gmail.com>
2024-12-20 07:59:29 +0530

    ...

--- components/Section6.jsx
+++ components/Section6.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Section6 = () => {
   return (
-    <div className='flex justify-around items-center  bg-white shadow-[0px_3px_15px_1px_rgba(54,43,43,0.15)] gap-2 max-w-[94%] px-4 mx-auto rounded-[15px] py-3'>
+    <div className='flex justify-around items-center  bg-white shadow-[0px_3px_15px_1px_rgba(54,43,43,0.15)] gap-2 max-w-[93%] px-1 mx-auto rounded-[15px] py-3'>
       <div>
         <img 
           src="/ppf.jpg" 
@@ -12,10 +12,10 @@ const Section6 = () => {
       </div>
       
       <div className='flex flex-col gap-[2px]'>
-        <p className='text-[14px] font-semibold leading-[20px] max-[700px]:leading-[16px] text-[#2F327D]'>
+        <p className='text-[14px] font-semibold leading-[20px] max-[700px]:leading-[18px] text-[#2F327D]'>
           Highly recommended if you're looking to get Ecom leads!
         </p>
-        <p className='text-[11px] font-normal leading-[12px] text-[#696984]'>
+        <p className='text-[11px] font-normal leading-[13px] text-[#696984]'>
           "Helps me scrape my target audience data with ease... Low effort, amazing results. Saves a lot of time and manual work"
         </p>
         <p className='text-[11px] font-medium text-[#2F327D]'>
---
+++ components/Section7.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section7 = () => {
+  return (
+    <div >
+      <p className='text-[38px] leading-[48px] text-white'>
+      Ready to close more deals for your business?
+      </p>
+    </div>
+  )
+}
+
+export default Section7
